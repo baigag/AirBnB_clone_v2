d0c99891ad973b584d2f9939fe4df6357413075b
Godfrey Baiga <106968031+baigag@users.noreply.github.com>
2023-03-22 22:33:05 +0300

    new updated files

--- console.py
+++ console.py
@@ -37,6 +37,7 @@ class HBNBCommand(cmd.Cmd):
 
     def precmd(self, line):
         """Reformat command line for advanced command syntax.
+
         Usage: <class name>.<command>([<id> [<*args> or <**kwargs>]])
         (Brackets denote optional fields in usage example.)
         """
@@ -327,3 +328,4 @@ class HBNBCommand(cmd.Cmd):
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
+
